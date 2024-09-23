46ed7041dbe32a17cebb357981e27ccc2b4f4267
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2024-09-23 06:42:53 +0200

    add loader to home page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,8 +5,7 @@ function Home() {
     <div>
       <main>
         <div className="container text-center pt-5">
-          <h1>Welcome to this template</h1>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto incidunt blanditiis suscipit neque illo qui exercitationem laborum, et tenetur itaque pariatur culpa, similique autem in labore ducimus aperiam fuga eveniet? Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur enim dolore soluta aliquid quidem, quisquam nisi dolorem incidunt magnam unde asperiores ex aperiam iure veritatis. Sit numquam ut deserunt magni!</p>
+          <div className="loader mx-auto mt-5"></div>
         </div>
       </main>
     </div>
