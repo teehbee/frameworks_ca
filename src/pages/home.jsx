df034a09f77e113d3dab910c1d7efa543215e165
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2024-09-23 06:47:55 +0200

    add error text to home page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,6 +5,7 @@ function Home() {
     <div>
       <main>
         <div className="container text-center pt-5">
+          <p className="error-text text-danger fw-medium pt-3 d-none">There was a problem filling the store. Please try again later. </p>
           <div className="loader mx-auto mt-5"></div>
         </div>
       </main>
