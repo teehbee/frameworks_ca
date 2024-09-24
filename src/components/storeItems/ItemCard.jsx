697722e314b5700fb91dcd730e79f3e735f73e62
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2024-09-24 10:36:15 +0200

    fetch and display products

--- src/components/storeItems/ItemCard.jsx
+++ src/components/storeItems/ItemCard.jsx
@@ -2,24 +2,40 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
-import { CardImage } from "../../assets/img";
+import useApi from "../../api/products";
 
-function ItemCard() {
+function ItemCard({ visibleCount }) {
+  const { data, isLoading, isError } = useApi("https://v2.api.noroff.dev/online-shop");
+
+  if (isLoading) {
+    return <div className="loader mx-auto mt-5"></div>;
+  }
+
+  if (isError) {
+    <p className="error-text text-danger fw-medium pt-3">There was a problem filling the store. Please try again later. </p>;
+  }
+
+  console.log("fetched data is: ", data);
+
   return (
-    <div className="col-6 col-md-4 col-lg-3 col-xl-2 mx-auto mb-3 ">
-      <Card className="product-card">
-        <Link to="product">
-          <Card.Img variant="top" className="mx-auto card-image" src={CardImage} />
-        </Link>
-        <Card.Body>
-          <Link className="card-link " to="product">
-            <Card.Title className="fs-0-75rem-to-1-rem link-text green-font-color">Product title</Card.Title>
-          </Link>
-          <Card.Text className="fs-0-625rem-to-0-75-rem">Price goes here</Card.Text>
-          <Button className="add-to-cart-button green-button fs-0-75rem py-2 px-3 px-sm-4 px-md-4 px-xl-5">Add to cart</Button>
-        </Card.Body>
-      </Card>
-    </div>
+    <>
+      {data.map((product) => (
+        <div key={product.id} className="col-6 col-md-4 col-lg-3 col-xl-2 mx-auto mb-3">
+          <Card className="product-card">
+            <Link to={`product/${product.id}`}>
+              <Card.Img variant="top" className="item-card-img img-fluid mx-auto card-image" src={product.image.url} />
+            </Link>
+            <Card.Body>
+              <Link className="card-link" to={`product/${product.id}`}>
+                <Card.Title className="fs-0-75rem-to-1-rem link-text green-font-color">{product.title}</Card.Title>
+              </Link>
+              <Card.Text className="fs-0-625rem-to-0-75-rem">{product.price}</Card.Text>
+              <Button className="add-to-cart-button green-button fs-0-75rem py-2 px-3 px-sm-4 px-md-4 px-xl-5">Add to cart</Button>
+            </Card.Body>
+          </Card>
+        </div>
+      ))}
+    </>
   );
 }
 
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { ItemCard } from "../components";
 
 function Home() {
+  // const [visibleCount, setVisibleCount] = useState(18);
+
+  // function loadMore() {
+  //   setVisibleCount((prevCount) => prevCount + 18);
+  // }
   return (
     <div>
       <main>
@@ -11,21 +16,7 @@ function Home() {
           </div>
           <div className="row">
             <ItemCard />
-            <ItemCard />
-            <ItemCard />
-            <ItemCard />
-            <ItemCard />
-            <ItemCard />
-            <ItemCard />
-            <ItemCard />
-            <ItemCard />
-            <ItemCard />
-            <ItemCard />
-            <ItemCard />
           </div>
-
-          <p className="error-text text-danger fw-medium pt-3 d-none">There was a problem filling the store. Please try again later. </p>
-          <div className="loader mx-auto mt-5 d-none"></div>
           <p className="home-load-items link-text fs-0-75rem-to-1-rem text-decoration-none py-4 ">Load more Støff.</p>
         </div>
       </main>
