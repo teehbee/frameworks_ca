5583e2623a13888cfa24a7d8f7862e50b08b90fd
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2024-09-30 10:09:52 +0200

    add clearCart functionality to purchase button

--- src/components/cartPage/CartButtonContainer.jsx
+++ src/components/cartPage/CartButtonContainer.jsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
+import { CartContext } from "../utils/cartContext";
 import { Link } from "react-router-dom";
 
 function CartButton() {
+  const { clearCart } = useContext(CartContext);
+
+  const handlePurchase = () => {
+    clearCart();
+  };
   return (
     <div className="cart-button-container text-center pb-2 pb-md-5">
       <Link to="/success" className="">
-        <button className="green-button w-100 my-3 my-md-5 py-1 py-md-2">Purchase items</button>
+        <button onClick={handlePurchase} className="green-button w-100 my-3 my-md-5 py-1 py-md-2">
+          Purchase items
+        </button>
       </Link>
 
       <Link to="/" className="link-text fs-0-75rem-to-1-rem main-font-colour">
