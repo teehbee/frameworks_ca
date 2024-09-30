1a80fc7edd87f4df105cd61f5b04e4775d919e5f
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2024-09-30 09:53:52 +0200

    add summation of cart

--- src/components/cartPage/CartSum.jsx
+++ src/components/cartPage/CartSum.jsx
@@ -1,6 +1,11 @@
-import React from "react";
+import React, { useContext } from "react";
+import { CartContext } from "../utils/cartContext";
+import Cart from "../../pages/cart";
 
 function CartSum() {
+  const { cart } = useContext(CartContext);
+
+  const totalSum = cart.reduce((sum, product) => sum + product.price, 0);
   return (
     <div className="cart-sum">
       <div className="row cart-top-border pt-3">
@@ -8,7 +13,7 @@ function CartSum() {
           <p className="fw-bold fs-1-to-1-125">Total</p>
         </div>
         <div className="col-2 col-md-2 ms-auto">
-          <p className="fs-0-75rem-to-1-rem fw-bold">$225</p>
+          <p className="fs-0-75rem-to-1-rem fw-bold">${totalSum.toFixed(2)}</p>
         </div>
         <div className="col-3 col-sm-2 col-md-1 text-end">
           <p className="fs-0-75rem-to-1-rem fw-bold">3 items</p>
