078d62a2f89560f91b044efe8773d648deeb0d30
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2024-10-01 15:04:41 +0200

    add privacy and cookies site to router

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -27,6 +27,10 @@ const router = createBrowserRouter([
         path: "success",
         element: <Success />,
       },
+      {
+        path: "privacy-and-cookies",
+        element: <Privacy />,
+      },
     ],
   },
   {
