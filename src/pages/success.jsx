9cc26dc18cde57ac0210ebaf0e5cd5e77c5e4f65
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2024-10-03 10:55:38 +0200

    add added to cart popup to single product page

--- src/pages/success.jsx
+++ src/pages/success.jsx
@@ -5,7 +5,7 @@ import successImage from "/src/assets/img/success-check.png";
 function Success() {
   return (
     <main>
-      <div className="container text-center pt-5">
+      <div className="container text-center py-5">
         <h1 className="fw-bold fs-1-25-to-2rem">Thank you for your purchase!</h1>
         <p className="mw-300px mx-auto fs-0-948rem pt-3 pt-lg-4">You will receive your purchase within 1-3 days</p>
         <img className="pt-3 pt-lg-4" src={successImage} alt="Image with okay symbol showing that your purchase was complete" />
