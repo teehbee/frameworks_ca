35f955e8a436fded8346ebfcc037f3565e90fcb2
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2024-10-04 06:20:44 +0200

    fix frontpage loader and padding on main

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -23,29 +23,35 @@ function Home() {
   const visibleData = data?.slice(0, visibleCount);
 
   if (isLoading) {
-    return <div className="loader mx-auto my-5"></div>;
+    return (
+      <main>
+        <div className="loader mx-auto my-5"></div>;
+      </main>
+    );
   }
 
   if (isError) {
-    return <p className="error-text text-danger fw-medium pt-3">There was a problem filling the store. Please try again later.</p>;
+    return (
+      <main>
+        <p className="error-text text-danger fw-medium pt-3">There was a problem filling the store. Please try again later.</p>
+      </main>
+    );
   }
 
   return (
-    <>
-      <main>
-        <div className="container text-center pt-5">
-          <div className="row">
-            <ItemCard data={visibleData} />
-          </div>
-          {visibleCount < data.length && (
-            <p onClick={handleLoadMore} className="home-load-items link-text fs-0-75rem-to-1-rem text-decoration-none py-4 ">
-              Load more Støff.
-            </p>
-          )}
+    <main className="frontpage-main-container">
+      <div className="container text-center pt-5">
+        <div className="row">
+          <ItemCard data={visibleData} />
         </div>
-        <CookieConsentPopup />
-      </main>
-    </>
+        {visibleCount < data.length && (
+          <p onClick={handleLoadMore} className="home-load-items link-text fs-0-75rem-to-1-rem text-decoration-none py-4 ">
+            Load more Støff.
+          </p>
+        )}
+      </div>
+      <CookieConsentPopup />
+    </main>
   );
 }
 
